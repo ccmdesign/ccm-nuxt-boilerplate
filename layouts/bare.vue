00ff9e5fec36a435765a4268943d29eceac8f0e6
Claudio <claudio@ccmdesign.ca>
2024-03-06 11:59:58 -0800

    position statement - test 1

---
+++ layouts/bare.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="page-wrapper">
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.page-wrapper {
+  min-height: 100vh;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+
+  main { flex: 1; }
+}
+</style>
